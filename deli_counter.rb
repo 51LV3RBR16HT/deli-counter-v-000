938561502ab62a08b9d584e25f9a1b5f590ba309
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 05:38:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,14 +6,14 @@ def line(katz_deli)
   else
      occupied_line= "The line is currently:"
      katz_deli.each.with_index(1) do |names, index|
-      occupied_line << " #{index}. #{names}"
+      occupied_line.push(" #{index}. #{names}")
     end
     puts occupied_line
   end
 end
 
 def take_a_number(katz_deli, name)
-  line(katz_deli) = names
+
 
 
   puts "Welcome, #{names}. You are number #{index} in line."
