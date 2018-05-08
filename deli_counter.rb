3f97bb1f804beb9c7ac10cd0f281b60498ec2c01
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 05:46:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    current_line= "The line is currently:"
+    current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, index|
     current_line << " #{index}. #{name}"
   end
