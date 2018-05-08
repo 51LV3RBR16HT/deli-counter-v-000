3318067c55b96ec778d270ebe091fdf644dc1baf
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 05:28:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
      occupied_line= "The line is currently:"
-     katz_deli.each.with_index(1) do |name, index|
-      occupied_line << " #{index}. #{name}"
+     katz_deli.each.with_index(1) do |names, index|
+      occupied_line << " #{index}. #{names}"
     end
     puts occupied_line
   end
